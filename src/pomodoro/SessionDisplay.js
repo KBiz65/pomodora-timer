88120f5a39b9e4181040c3fa437e6a651143f764
Kevin Bisner <34324949+KBiz65@users.noreply.github.com>
2021-07-23 12:43:57 -0700

    remove unused code

--- src/pomodoro/SessionDisplay.js
+++ src/pomodoro/SessionDisplay.js
@@ -4,18 +4,6 @@ import SessionText from "./SessionText";
 import ProgressBarDisplay from "./ProgressBarDisplay";
 
 function SessionDisplay({ session, focusDuration, breakDuration }) {
-  let ariaNow = 100;
-  let ariaWidth = 0;
-  if (session) {
-    if (session.label === "Focusing") {
-      ariaNow = session.timeRemaining / (focusDuration * 60);
-      ariaWidth = (1 - ariaNow) * 100;
-    } else {
-      ariaNow = session.timeRemaining / (breakDuration * 60);
-      ariaWidth = (1 - ariaNow) * 100;
-    }
-  }
-
   return (
     session && (
       <>
